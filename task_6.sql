04488cf0f628b2708e04f0b9b7b8e193f9ddfa49
Isac Yevisa <isacyevisa@gmail.com>
2025-07-01 11:36:33 +0300

    modified the script

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,3 @@
--- task_6.sql
-
-USE alx_book_store;  -- Select the database
-
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
