945a53990b4709537a43fd772319d24a4d1feae0
Isac Yevisa <isacyevisa@gmail.com>
2025-07-01 10:18:01 +0300

    Add SQL script to insert a single row in customers table

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- task_5.sql
+
+USE alx_book_store;  -- Select the database
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
